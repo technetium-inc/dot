ba1412134b680ce3971a7b93ea34b0b337049ff2
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-06-17 20:24:20 +0530

    fmt:Formatted the code using `astyle`

--- exception.h
+++ exception.h
@@ -1,32 +1,37 @@
 #ifndef EXCEPTION_H
-#define EXCEPTION_H	
+#define EXCEPTION_H
 
-class Exception {
-	private:
-	std::string message;
-	std::string suggestion;
-	bool isFatal;
-	int exitStatusCode;
+class Exception
+{
+private:
+    std::string message;
+    std::string suggestion;
+    bool isFatal;
+    int exitStatusCode;
 
-	void throwException() {
-		std::cout << this->message << std::endl;
-		if(this->suggestion.length() > 0){
-			std::cout << this->suggestion << std::endl;
-		}
-		if(this->isFatal){
-			exit(this->exitStatusCode);
-		}
-	}
+    void throwException()
+    {
+        std::cout << this->message << std::endl;
+        if(this->suggestion.length() > 0)
+        {
+            std::cout << this->suggestion << std::endl;
+        }
+        if(this->isFatal)
+        {
+            exit(this->exitStatusCode);
+        }
+    }
 
-	public:
-	Exception(std::string message, std::string suggestion="", bool isFatal=true, int exitStatusCode=1){
-		this->message = message;
-		this->suggestion = suggestion;
-		this->isFatal = isFatal;
-		this->exitStatusCode = exitStatusCode;
+public:
+    Exception(std::string message, std::string suggestion="", bool isFatal=true, int exitStatusCode=1)
+    {
+        this->message = message;
+        this->suggestion = suggestion;
+        this->isFatal = isFatal;
+        this->exitStatusCode = exitStatusCode;
 
-		this->throwException();
-	}
+        this->throwException();
+    }
 };
 
 #endif
